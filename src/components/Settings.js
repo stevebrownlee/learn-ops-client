ae3da6a71903e3ae4dcd1985f7aecc01e42ed197
Steve Brownlee <chortlehoort@gmail.com>
2021-12-10 06:08:19 -0600

    Trying env files for settings

--- src/components/Settings.js
+++ src/components/Settings.js
@@ -1,4 +1,4 @@
 export default {
-    apiHost: "https://learningapi.nss.team"
-    // apiHost: "http://localhost:8000"
+    apiHost: process.env.REACT_APP_API_URI,
+    redirect_uri: process.env.REACT_APP_ENV === "development" ? `${process.env.REACT_APP_API_URI}/auth/github/callback` : null
 }
--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -1,23 +1,12 @@
-import React, { useRef, useState } from "react"
-import { useHistory } from "react-router-dom"
-import useSimpleAuth from "./useSimpleAuth"
+import React from "react"
 import Settings from "../Settings"
 import "./Auth.css"
 
-export const Login = () => {
-    const invalidDialog = useRef()
-    const history = useHistory()
-    const [token, setToken] = useState(null)
-    const [profile, updateProfile] = useState({})
-    const { login, storeCurrentUser, logout } = useSimpleAuth()
+console.log(Settings)
 
-
-    return (
-        <article className="container--login">
-            <h1>NSS Learn Ops</h1>
-            <a href={`${Settings.apiHost}/auth/github/url`} className="button bg-2 button--size-l button--round-m button--border-medium btn-github ">
-                <i className="fa fa-github"></i> Sign in with Github
-            </a>
-        </article>
-    )
-}
+export const Login = () => <article className="container--login">
+    <h1>NSS Learn Ops</h1>
+    <a href={`${Settings.apiHost}/auth/github/url`} className="button bg-2 button--size-l button--round-m button--border-medium btn-github ">
+        <i className="fa fa-github"></i> Sign in with Github
+    </a>
+</article>
