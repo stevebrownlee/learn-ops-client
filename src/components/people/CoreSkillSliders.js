7a3bff40272c874b30948d7611eebb269d21c462
Steve <chortlehoort@gmail.com>
2022-04-09 22:05:18 -0500

    Can now add notes when changing core skill level

--- src/components/people/CoreSkillSliders.js
+++ src/components/people/CoreSkillSliders.js
@@ -1,22 +1,37 @@
-import React, { useContext } from "react"
+import React, { useContext, useEffect, useState } from "react"
 import { PeopleContext } from "./PeopleProvider"
-import { fetchIt } from "../utils/Fetch"
 import Settings from "../Settings"
-import { useHistory } from "react-router-dom"
+import useModal from "../ui/useModal"
+import { fetchIt } from "../utils/Fetch"
 import "./CoreSkills.css"
 
 export const CoreSkillSliders = () => {
     const { activeStudent, getStudent } = useContext(PeopleContext)
+    const [note, setNote] = useState("")
+    const [changedRecord, updateChangedRecord] = useState(0)
+    let { toggleDialog: toggleCoreSkillNote } = useModal("#dialog--coreskillNote")
+
+    const reset = () => {
+        setNote("")
+        toggleCoreSkillNote()
+    }
+
+    useEffect(() => {
+        if (changedRecord > 0) {
+            toggleCoreSkillNote()
+            document.getElementById("coreskillNoteText").focus()
+        }
+    }, [changedRecord])
 
     return <>
-        {
-            activeStudent.core_skill_records.length
-                ? activeStudent.core_skill_records.map(
-                    record => <section key={`coreskill--${record.id}`}>
-                        <h4>{record.skill.label} ({record.level})</h4>
-                        <div className="slidecontainer">
+        <div className="sliders">
+            {
+                activeStudent.core_skill_records.length
+                    ? activeStudent.core_skill_records.map(
+                        record => <section className="slider" key={`coreskill--${record.id}`}>
+                            <i className="icon icon-eye icon--more"></i> <h4 className="slider__header">{record.skill.label} ({record.level})</h4>
                             <input type="range" min="1" max="10" defaultValue={record.level}
-                                className="slider slider--coreSkill"
+                                className="slider__range slider--coreSkill"
                                 id={`record--${record.id}`}
                                 onMouseUp={(e) => {
                                     const updatedRecord = {
@@ -27,49 +42,76 @@ export const CoreSkillSliders = () => {
                                     fetchIt(`${Settings.apiHost}/coreskillrecords/${record.id}`, {
                                         method: "PUT",
                                         body: JSON.stringify(updatedRecord)
-                                    }).then(getStudent)
+                                    }).then(() => {
+                                        updateChangedRecord(record.id)
+                                        // getStudent()
+                                    })
                                 }}
                             />
-                        </div>
-                    </section>
-                )
-                : <>
-                    <button className="button button--isi button--border-thick button--round-l button--size-s button--assessment"
-                        onClick={() => {
-                            fetchIt(`${Settings.apiHost}/coreskills`)
-                                .then(skills => {
-                                    const newRecordRequests = []
+                        </section>
+                    )
+                    : <>
+                        <button className="button button--isi button--border-thick button--round-l button--size-s button--assessment"
+                            onClick={() => {
+                                fetchIt(`${Settings.apiHost}/coreskills`)
+                                    .then(skills => {
+                                        const newRecordRequests = []
 
-                                    for (const skill of skills.results) {
-                                        newRecordRequests.push(
-                                            fetchIt(`${Settings.apiHost}/coreskillrecords`, {
-                                                method: "POST",
-                                                body: JSON.stringify({
-                                                    "student": activeStudent.id,
-                                                    "skill": skill.id,
-                                                    "note": "Initial record",
-                                                    "level": 1
+                                        for (const skill of skills.results) {
+                                            newRecordRequests.push(
+                                                fetchIt(`${Settings.apiHost}/coreskillrecords`, {
+                                                    method: "POST",
+                                                    body: JSON.stringify({
+                                                        "student": activeStudent.id,
+                                                        "skill": skill.id,
+                                                        "note": "Initial record",
+                                                        "level": 1
+                                                    })
                                                 })
-                                            })
-                                        )
-                                    }
-                                    Promise.all(newRecordRequests).then(getStudent)
-                                })
-                        }}>
-                        <i className="button__icon icon icon-book"></i>
-                        <span>Start Tracking Core Skills</span>
-                    </button>
-                </>
-        }
+                                            )
+                                        }
+                                        Promise.all(newRecordRequests).then(getStudent)
+                                    })
+                            }}>
+                            <i className="button__icon icon icon-book"></i>
+                            <span>Start Tracking Core Skills</span>
+                        </button>
+                    </>
+            }
+        </div>
+
+
+
+
+        <dialog id="dialog--coreskillNote" className="dialog--coreskillNote">
+            <div className="form-group">
+                <label htmlFor="name">Why are you changing core skill?</label>
+                <input type="text" id="coreskillNoteText"
+                    className="form-control form-control--dialog"
+                    value={note}
+                    onChange={e => setNote(e.target.value)}
+                    onKeyDown={
+                        e => {
+                            if (e.key === "Enter") {
+                                if (e.target.value !== "") {
+                                    fetchIt(`${Settings.apiHost}/coreskillrecords/entries`, {
+                                        method: "POST",
+                                        body: JSON.stringify({ record: changedRecord, note })
+                                    }).then(() => {
+                                        getStudent()
+                                        reset()
+                                    })
+                                }
+                                else {
+                                    reset()
+                                }
+                            }
+                            else if (e.key === "Escape") {
+                                reset()
+                            }
+                        }
+                    } />
+            </div>
+        </dialog>
     </>
 }
-
-
-/*
-    {
-        "student": 17,
-        "skill": 4,
-        "note": "First evaluation",
-        "level": 7
-    }
-*/
