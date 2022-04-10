0824f689a78926013179457ce35cc06812baf683
Steve <chortlehoort@gmail.com>
2022-04-10 11:04:18 -0500

    Can now view core skill notes history with eye icon

--- src/components/people/CoreSkillSliders.js
+++ src/components/people/CoreSkillSliders.js
@@ -4,17 +4,31 @@ import Settings from "../Settings"
 import useModal from "../ui/useModal"
 import { fetchIt } from "../utils/Fetch"
 import "./CoreSkills.css"
+import { HumanDate } from "../utils/HumanDate"
 
 export const CoreSkillSliders = () => {
     const { activeStudent, getStudent } = useContext(PeopleContext)
     const [note, setNote] = useState("")
     const [changedRecord, updateChangedRecord] = useState(0)
+    const [skillRecords, setSkillRecords] = useState([])
+    const [chosenCoreSkill, setChosenCoreSkill] = useState(0)
     let { toggleDialog: toggleCoreSkillNote } = useModal("#dialog--coreskillNote")
+    let { toggleDialog: toggleSkillHistory } = useModal("#dialog--coreskillHistory")
 
     const reset = () => {
         setNote("")
         toggleCoreSkillNote()
     }
+
+    useEffect(() => {
+        if (chosenCoreSkill !== 0) {
+            const coreSkill = activeStudent.core_skill_records
+                .find(record => record.id === chosenCoreSkill)
+
+            setSkillRecords(coreSkill.notes.map(n => ({ ...n })).reverse())
+        }
+
+    }, [chosenCoreSkill])
 
     useEffect(() => {
         if (changedRecord > 0) {
@@ -29,7 +43,13 @@ export const CoreSkillSliders = () => {
                 activeStudent.core_skill_records.length
                     ? activeStudent.core_skill_records.map(
                         record => <section className="slider" key={`coreskill--${record.id}`}>
-                            <i className="icon icon-eye icon--more"></i> <h4 className="slider__header">{record.skill.label} ({record.level})</h4>
+                            <i className="icon icon-eye icon--more"
+                                onClick={e => {
+                                    setChosenCoreSkill(record.id)
+                                    toggleSkillHistory()
+                                }}
+                            ></i>
+                            <h4 className="slider__header">{record.skill.label} ({record.level})</h4>
                             <input type="range" min="1" max="10" defaultValue={record.level}
                                 className="slider__range slider--coreSkill"
                                 id={`record--${record.id}`}
@@ -44,7 +64,6 @@ export const CoreSkillSliders = () => {
                                         body: JSON.stringify(updatedRecord)
                                     }).then(() => {
                                         updateChangedRecord(record.id)
-                                        // getStudent()
                                     })
                                 }}
                             />
@@ -113,5 +132,33 @@ export const CoreSkillSliders = () => {
                     } />
             </div>
         </dialog>
+
+        <dialog style={{ paddingTop: "1rem"}} id="dialog--coreskillHistory" className="dialog--coreskillHistory">
+            {
+                skillRecords.reverse().map(
+                    note => {
+                        return <React.Fragment key={`skillNote--${note.id}`}>
+                            <div className="status">
+                                <div className="status__note"> {note.note} </div>
+                                <div className="status__date">
+                                    Recorded on <HumanDate date={note.recorded_on.split("T")[0]} /> by {note.instructor}
+                                </div>
+                            </div>
+                            <div className="status__separator"></div>
+                        </React.Fragment>
+                    }
+                )
+            }
+            <button className="fakeLink" style={{
+                position: "absolute",
+                top: "0.33em",
+                right: "0.5em",
+                fontSize: "0.75rem"
+            }}
+                id="closeBtn"
+                onClick={toggleSkillHistory}>[ close ]</button>
+        </dialog>
+
+
     </>
 }
