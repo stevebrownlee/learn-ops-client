ce7640db57a45f35238205a545575b77040ef8be
Steve <chortlehoort@gmail.com>
2023-02-12 09:31:19 -0600

    Made container for all assessments

---
+++ src/components/people/StudentAssessments.js
@@ -0,0 +1,17 @@
+import React, { useContext, useState } from "react"
+import { StudentProposals } from "./StudentProposals"
+
+export const StudentAssessments = () => {
+
+
+    return <>
+        <div style={{margin: "0 0 0 2rem"}} className="card-title">
+            <h2>Assessments</h2>
+        </div>
+
+        <div className="card card--row">
+            <StudentProposals />
+            <StudentProposals />
+        </div>
+    </>
+}
--- src/components/people/StudentDetails.js
+++ src/components/people/StudentDetails.js
@@ -6,6 +6,7 @@ import { PeopleContext } from "./PeopleProvider.js"
 import { StudentPersonality } from "./StudentPersonality.js"
 import { StudentProposals } from "./StudentProposals.js"
 import "../people/Status.css"
+import { StudentAssessments } from "./StudentAssessments.js"
 
 export const StudentDetails = ({ toggleCohorts }) => {
     const { activeStudent } = useContext(PeopleContext)
@@ -21,62 +22,56 @@ export const StudentDetails = ({ toggleCohorts }) => {
         }
     }, [activeStudent])
 
-    return (
-        <>
-            <div className="overlay--student">
-                <div className="card" style={{
-                    display: "flex",
-                    alignItems: "center",
-                    padding: "1rem 0 0 0"
-                }}>
-                    <span onClick={hideOverlay} className="close hairline"></span>
-                </div>
-                <div className="card">
-                    <div className="card-body">
-                        <header className="student__header">
-                            <h2 className="card-title student__info">
-                                {activeStudent?.name}
-                            </h2>
-                            <div className="student__score">
-                                {activeStudent?.score}
-                            </div>
-                        </header>
-                        <div className="card-text">
-                            <div className="student__details">
+    return <div className="overlay--student">
+        <div className="card" style={{
+            display: "flex",
+            alignItems: "center",
+            padding: "1rem 0 0 0"
+        }}>
+            <span onClick={hideOverlay} className="close hairline"></span>
+        </div>
+        <div className="card">
+            <div className="card-body">
+                <header className="student__header">
+                    <h2 className="card-title student__info">
+                        {activeStudent?.name}
+                    </h2>
+                    <div className="student__score">
+                        {activeStudent?.score}
+                    </div>
+                </header>
+                <div className="card-text">
+                    <div className="student__details">
 
-                                <div className="student__github">
-                                    Github: <a href={`https://www.github.com/${activeStudent?.github}`}>
-                                        {`https://www.github.com/${activeStudent?.github}`}</a>
-                                </div>
-                                <div className="student__cohort">
-                                    Cohort: <button className="fakeLink"
-                                        onClick={() => {
-                                            toggleCohorts()
-                                        }}>
-                                        {activeStudent?.cohorts?.map(c => c.name).join(", ")}
-                                    </button>
-                                </div>
-                            </div>
+                        <div className="student__github">
+                            Github: <a href={`https://www.github.com/${activeStudent?.github}`}>
+                                {`https://www.github.com/${activeStudent?.github}`}</a>
+                        </div>
+                        <div className="student__cohort">
+                            Cohort: <button className="fakeLink"
+                                onClick={() => {
+                                    toggleCohorts()
+                                }}>
+                                {activeStudent?.cohorts?.map(c => c.name).join(", ")}
+                            </button>
                         </div>
                     </div>
                 </div>
-
-                <div className="card">
-                    <CoreSkillSliders />
-                </div>
-
-                <div className="card">
-                    <LearningObjectives />
-                </div>
-
-                <div className="card">
-                    <StudentProposals />
-                </div>
-
-                <div className="card">
-                    <StudentPersonality />
-                </div>
             </div>
-        </>
-    )
+        </div>
+
+        <div className="card">
+            <CoreSkillSliders />
+        </div>
+
+        <div className="card">
+            <LearningObjectives />
+        </div>
+
+        <StudentAssessments />
+
+        <div className="card">
+            <StudentPersonality />
+        </div>
+    </div>
 }
