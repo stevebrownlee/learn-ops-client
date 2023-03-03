0eeaa0e19eb7ed062aaeed818ab2dcadb5018b36
Steve <chortlehoort@gmail.com>
2023-03-03 17:04:02 -0600

    Revert login button

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -12,7 +12,7 @@ export const Login = () => <article className="container--login">
         Please make sure you have created an account on Github, and entered your name in your <a href="https://github.com/settings/profile">account profile</a> before accessing the Learning Platform.
     </div>
 
-    <a href={`${Settings.apiHost}/auth/github/url?cohort=64&v=1`} className="button bg-2 button--size-l button--round-m button--border-medium btn-github ">
+    <a href={`${Settings.apiHost}/auth/github/url`} className="button bg-2 button--size-l button--round-m button--border-medium btn-github ">
         <i className="fa fa-github"></i> Sign in with Github
     </a>
 </article>
