5ad11b4b82be0a56ff4a373f79354be342a4cb25
Steve Brownlee <chortlehoort@gmail.com>
2024-01-26 19:03:09 -0600

    Showing bar chart and bubble chart

--- src/components/course/CourseDetailsChart.js
+++ src/components/course/CourseDetailsChart.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import { Bar } from 'react-chartjs-2'
+import { Bar, Bubble } from 'react-chartjs-2'
 import { fetchIt } from '../utils/Fetch.js'
 import Settings from '../Settings.js'
 import {
@@ -7,6 +7,7 @@ import {
     CategoryScale,
     LinearScale,
     BarElement,
+    PointElement,
     Title,
     Tooltip,
     Legend
@@ -16,21 +17,30 @@ ChartJS.register(
     CategoryScale,
     LinearScale,
     BarElement,
+    PointElement,
     Title,
     Tooltip,
     Legend
 )
 
 export const CourseDetailsChart = ({ courseId }) => {
-    const [chartData, setChartData] = useState({
+    const [barChartData, setBarChartData] = useState({
         labels: ["Project"],
         datasets: [
             {
                 label: 'Average Start Delay (days)',
-                data: [1],
+                data: [0],
                 backgroundColor: 'rgba(54, 162, 235, 0.5)',
                 borderColor: 'rgba(54, 162, 235, 1)',
                 borderWidth: 1,
+            },
+        ],
+    })
+    const [chartData, setChartData] = useState({
+        labels: ["Project"],
+        datasets: [
+            {
+                x: 0, y: 0, r: 0
             },
         ],
     })
@@ -38,7 +48,7 @@ export const CourseDetailsChart = ({ courseId }) => {
     useEffect(() => {
         fetchIt(`${Settings.apiHost}/courses/${courseId}/stats`)
             .then((response) => {
-                const chartData = {
+                let chartData = {
                     labels: response.data.map(item => item.projectname),
                     datasets: [
                         {
@@ -51,17 +61,78 @@ export const CourseDetailsChart = ({ courseId }) => {
                     ],
                 }
 
+                setBarChartData(chartData)
+
+                chartData = {
+                    datasets: [
+                        {
+                            label: ["Time on Project"],
+                            data: response.data.map((item, idx, arr) => {
+                                // debugger
+                                item.x = idx + 1
+                                item.y = item.averagestartdelay
+                                try {
+                                    item.r = Math.abs(idx < arr.length ? arr[idx + 1].averagestartdelay - arr[idx].averagestartdelay : 1)
+                                } catch (error) {
+                                    item.r = 1
+                                }
+                                item.r *= 3
+                                item.label = item.projectname
+
+                                return item
+                            }),
+                            backgroundColor: 'rgba(149, 183, 68, 0.5)',
+                        }
+                    ],
+                }
+
                 setChartData(chartData)
             })
     }, [])
 
-    return <Bar data={chartData}
-        options={{
-            scales: {
-                y: {
-                    beginAtZero: true
-                }
-            }
-        }}
-    />
+    return <div style={{ display: "flex", justifyContent: 'space-between' }}>
+        <div style={{ width: "45%" }}>
+            <Bar data={barChartData}
+                options={{
+                    scales: {
+                        y: {
+                            beginAtZero: true
+                        }
+                    }
+                }}
+            />
+        </div>
+        <div style={{ width: "45%" }}>
+            <Bubble data={chartData}
+                options={{
+                    scales: {
+                        y: {
+                            beginAtZero: true
+                        },
+                        x: {
+                            display: false
+                        }
+                    },
+                    plugins: {
+                        tooltip: {
+                            enabled: true,
+                            callbacks: {
+                                label: function (context) {
+                                    let label = context.dataset.label || '';
+
+                                    if (label) {
+                                        label += ': ';
+                                    }
+                                    if (context.parsed.y !== null) {
+                                        label += new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' }).format(context.parsed.y);
+                                    }
+                                    return `${context.raw.projectname}: ${Math.round(context.raw.r / 3)} days`;
+                                }
+                            }
+                        }
+                    }
+                }}
+            />
+        </div>
+    </div>
 }
