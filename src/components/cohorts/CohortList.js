ea39d2c03c2f6f12f1606fe8d49db5016e93da84
Steve Brownlee <chortlehoort@gmail.com>
2025-05-10 11:47:59 -0400

    Get 12 cohorts instead of 6

--- src/components/cohorts/CohortList.js
+++ src/components/cohorts/CohortList.js
@@ -24,7 +24,7 @@ export const CohortList = () => {
         getRecentCohorts()
     }, [])
 
-    const getRecentCohorts = () => getCohorts({ limit: 6 })
+    const getRecentCohorts = () => getCohorts({ limit: 12 })
 
     return <>
         <header className="cohorts__header">
